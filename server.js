76336563f96f72a0a07da775308d2c9a9851fa06
Kr1shnam00rthi <plkrishnamoorthi2004@gmail.com>
2025-07-30 23:50:29 +0530

    Add server.js

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const csrfMiddleware = require("./middlewares/csrfProtection");
+const path = require("path");
+
+const app = express();
+const PORT = 5001;
+
+app.use(cookieParser());
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "assets")));
+
+app.get("/", csrfMiddleware, (req, res) => {
+    res.sendFile(path.join(__dirname, "assets", "index.html"));
+});
+
+app.post("/api/submit-form", csrfMiddleware, (req, res) => {
+    const {username} = req.body;
+    console.log("Received username: ",username);
+    res.json({ message: "Form submitted successfully!" });
+})
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on http://localhost:${PORT}`)
+});
+
